b62052c3098c618b867b2d102c817dead65a1da6
Alexandre Jablon <alexandre.jablon@gmail.com>
2017-08-14 19:49:59 +0200

    Fix out-of-bound ips, improve code, accuracy

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const { ipToNum } = require('./util')
 
 function find (ip) {
   const num = ipToNum(ip)
+  if (num < ips[0][0] || num > ips[ips.length-1][0]) {
+    return null
+  }
 
   let min = 0
   let max = ips.length - 1
   let n
 
-  while (Math.abs(max - min) > 2) {
-    const m = min + Math.floor((max - min)/2)
+  while ((max - min) > 1) {
+    const m = min + Math.ceil((max - min)/2)
     n = ips[m]
 
-    if (num < n[0]) {
-      max = m
-    } else {
+    if (num >= n[0]) {
       min = m
+    } else {
+      max = m
     }
   }
 
